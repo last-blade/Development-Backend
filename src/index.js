8183b74429299b5bedcb0d102e2851d648b19043
last-blade <meprashanttyagi2000@gmail.com>
2025-04-22 14:26:45 +0530

    mongodb connection function added in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("Hello")
+import { app } from "./app.js";
+import { connectDB } from "./database.js";
+
+const PORT = process.env.PORT || 8000;
+
+connectDB()
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is listening on http://localhost:${PORT}`)
+    })
+})
+.catch((error) => {
+    console.log(`Database connection failed!!! ${error.message}`);
+});
