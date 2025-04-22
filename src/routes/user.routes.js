0c11275d675e58f32dc8d6767b51454ed1807abf
last-blade <meprashanttyagi2000@gmail.com>
2025-04-22 15:58:57 +0530

    added user routes

---
+++ src/routes/user.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { loginUser } from "../controllers/userControllers/loginUser.controller.js";
+import { registerUser } from "../controllers/userControllers/registerUser.controller.js";
+import { logoutUser } from "../controllers/userControllers/logoutUser.controller";
+import { authentication } from "../middlewares/authentication.middleware.js";
+
+const router = Router();
+
+router.route("/login").post(authentication, loginUser);
+router.route("/register").post(authentication, registerUser);
+router.route("/logout").post(authentication, logoutUser);
+
+export default router;
