f5ce9163d48c765a65d9709b4796dafa9aac67dd
last-blade <meprashanttyagi2000@gmail.com>
2025-04-22 15:14:55 +0530

    login-controller || Added

---
+++ src/controllers/userControllers/login.controller.js
@@ -0,0 +1,34 @@
+import { generateAccessToken } from "../../utils/generateAccessToken.js";
+import { apiError, apiResponse, asyncHandler, User } from "../allImports.js";
+
+const login = asyncHandler(async (request, response) => {
+    const {email, password} = request.body;
+
+    if(!email || !password){
+        throw new apiError(404, "All fields are required")
+    }
+
+    const foundUser = await User.findOne({email}).select("-password -accessToken");
+
+    if(!foundUser){
+        throw new apiError(404, "User with this email does not exists")
+    }
+
+    const userId = foundUser._id;
+
+    const accessToken = generateAccessToken(userId);
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+        sameSite: "None",
+    }
+
+    return response.status(200)
+    .cookie("accessToken", accessToken, options)
+    .json(
+        new apiResponse(200, foundUser, "Login successfully")
+    )
+});
+
+export {login}
