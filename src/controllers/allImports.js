54820f8fdc6df6796e74ea0159af28c5777b6cd8
last-blade <meprashanttyagi2000@gmail.com>
2025-04-22 15:11:53 +0530

    added allImports

---
+++ src/controllers/allImports.js
@@ -0,0 +1,7 @@
+import { apiError } from "../utils/apiError.js";
+import { apiResponse } from "../utils/apiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
+import { Client } from "../models/clientData.model.js";
+
+export {apiError, apiResponse, asyncHandler, User, Client}
